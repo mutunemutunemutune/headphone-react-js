e10decd4b07968f470acd6c583093aaa7daefb6c
mutunemutunemutune <vqeeh1234@gmail.com>
2024-08-14 17:51:59 +0300

    commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/my-project/",
+
   plugins: [react()],
+  base:"/headphone-react-js /",
 })
